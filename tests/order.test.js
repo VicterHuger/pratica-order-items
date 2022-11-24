7a8f51d928a6c63d51a91a4364fa6bc8fd0660dd
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-11-24 14:19:12 -0300

    test: :white_check_mark: use categories classes instead of Item class

--- tests/order.test.js
+++ tests/order.test.js
@@ -1,22 +1,22 @@
-import Item from "./../src/Item";
-import Order from "./../src/Order";
+import { Beer, Cigar, Eletronics, Water } from "../src/Category.js";
+import Order from "./../src/Order.js";
 
 test("Deve criar um pedido e calcular o total", () => {
   const order = new Order();
-  order.addItem(new Item("Beer", "Brahma", 6));
-  order.addItem(new Item("Cigar", "Malboro", 10));
-  order.addItem(new Item("Eletronics", "IPhone 13", 13000));
-  order.addItem(new Item("Water", "Crystal", 1));
+  order.addItem(new Beer("Brahma", 6));
+  order.addItem(new Cigar("Malboro", 10));
+  order.addItem(new Eletronics("IPhone 13", 13000));
+  order.addItem(new Water("Crystal", 1));
   const total = order.getTotal();
   expect(total).toBe(13017);
 });
 
 test("Deve criar um pedido e calcular os impostos", () => {
   const order = new Order();
-  order.addItem(new Item("Beer", "Brahma", 6));
-  order.addItem(new Item("Cigar", "Malboro", 10));
-  order.addItem(new Item("Eletronics", "IPhone 13", 13000));
-  order.addItem(new Item("Water", "Crystal", 1));
+  order.addItem(new Beer("Brahma", 6));
+  order.addItem(new Cigar("Malboro", 10));
+  order.addItem(new Eletronics("IPhone 13", 13000));
+  order.addItem(new Water("Crystal", 1));
   const taxes = order.getTaxes();
   expect(taxes).toBe(3903.7);
 });
