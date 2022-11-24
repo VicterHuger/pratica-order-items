4fadcc7f3267ea8728bd069b42ab89ffb9a19619
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-11-23 22:47:46 -0300

    feat: :sparkles: add a method to calculate the taxes of the items

--- src/Order.js
+++ src/Order.js
@@ -13,4 +13,18 @@ export default class Order {
     }, 0);
   }
 
+  getTaxes() {
+    const categoryTaxes = {
+      "Beer": 0.2,
+      "Cigar": 0.25,
+      "Eletronics": 0.3,
+      "Water": 0
+    };
+
+    return this.items.reduce((prev, curr) => {
+      return prev + curr?.price * categoryTaxes[curr?.category];
+    }, 0);
+  }
+
+
 }
