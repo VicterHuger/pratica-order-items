6a08cffa35f4d34ecf1e771f71bdf6c3d3021345
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-11-23 22:44:45 -0300

    feat: :sparkles: add Item class with category, description and price as properties

---
+++ src/Item.js
@@ -0,0 +1,11 @@
+export default class Item {
+  category;
+  description;
+  price;
+  constructor(category, description, price) {
+    this.category = category;
+    this.description = description;
+    this.price = price;
+  }
+
+}
