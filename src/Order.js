f4dc4f527cc9c185b2677fee94691ed07f340bda
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-11-24 13:18:53 -0300

    feat: :sparkles: calculate taxes by taxValue property of taxItem object

--- src/Order.js
+++ src/Order.js
@@ -14,15 +14,8 @@ export default class Order {
   }
 
   getTaxes() {
-    const categoryTaxes = {
-      "Beer": 0.2,
-      "Cigar": 0.25,
-      "Eletronics": 0.3,
-      "Water": 0
-    };
-
     return this.items.reduce((prev, curr) => {
-      return prev + curr?.price * categoryTaxes[curr?.category];
+      return prev + curr?.taxValue;
     }, 0);
   }
 
