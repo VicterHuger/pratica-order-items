86abc23dd49817695497e2f09de86960de107937
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-11-29 08:50:32 -0300

    refactor: :recycle: add a new file to represent Beer Class

---
+++ src/Beer.js
@@ -0,0 +1,12 @@
+import TaxItem from "./TaxItem";
+
+export default class Beer extends TaxItem {
+  constructor(description, price) {
+    super("Beer", description, price);
+  }
+
+  getTax() {
+    return 0.2;
+  }
+}
+
