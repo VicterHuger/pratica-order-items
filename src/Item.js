374d98657e396ba6755741a869229b005418133b
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-11-29 08:59:33 -0300

    refactor: :lock: turn Item class into an abstract class

--- src/Item.js
+++ src/Item.js
@@ -1,26 +1,9 @@
 export default class Item {
-  description;
-  price;
-  constructor(description, price) {
+  constructor(category, description, price) {
+    if (this.constructor === Item) throw new Error("Item classes can not be instantiated");
+    this.category = category;
     this.description = description;
     this.price = price;
-  }
-
-}
-
-export class TaxItem extends Item {
-  description;
-  price;
-  taxValue;
-  constructor(description, price) {
-    super(description, price);
-    this.description = description;
-    this.price = price;
-    this.taxValue = 0;
-  }
-
-  getTax(tax) {
-    return this.taxValue = this.price * tax;
   }
 
 }
